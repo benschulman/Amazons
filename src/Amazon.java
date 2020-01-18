24be6823a91832347cb75a38b7aa13e0bcf0fe34
benschulman <bgschulman31@gmail.com>
2020-01-18 01:17:29 -0600

    Added Config.java

--- src/Amazon.java
+++ src/Amazon.java
@@ -13,20 +13,8 @@ public class Amazon {
 	 * and 4 respectively.
 	 */
 	public Amazon() {
-		board = new char[6][6];
-		for(int i = 0; i < board.length; i++)
-			for(int j = 0; j < board[0].length; j++)
-				board[i][j] = ' ';
-
-		positions = new int[][] { {0,1}, {0,4}, {5,1}, {5,4} };
-
-		int count = 0;
-		for(int i = 0; i < positions.length; i++) {
-				int r = positions[i][0];
-				int c = positions[i][1];
-
-				board[r][c] = (char) (++count + 48) ;
-		}
+		this.board = Config.board1;
+		this.positions = Config.positions1;
 
 		turn = 0;
 	}
